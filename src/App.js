3ba96f891fb1f7a6ca3e9a9779133e51926a55a8
Joshua-Simon <joshy.d.simon@gmail.com>
2020-08-16 16:15:38 +1000

    increment working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+//to access a piece of state:
+import {useSelector, useDispatch} from 'react-redux'
+import {increment} from './actions'
+
 
 function App() {
+  const counter = useSelector(state =>state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  //this is how state is extracted from the store
+  const dispatch = useDispatch()
   return (
     <div>
-      test
+      <h1>Counter {counter}</h1>
+      <button onClick = {() => dispatch(increment())}>+</button>
+      <button>-</button>
+      {isLogged ?
+      <h3>valuable information i shouldn't see</h3>
+      :
+      ''
+      }
     </div>
   );
 }
