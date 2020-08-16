255a2db45e51abefc9cdda90da7ec49053b985c6
Joshua-Simon <joshy.d.simon@gmail.com>
2020-08-16 15:55:04 +1000

    problems fixed

--- src/index.js
+++ src/index.js
@@ -6,36 +6,11 @@ import allReducers from './reducers'
 import {Provider} from 'react-redux'
 
 
-
-//ACTION
-
-const increment = () => {
-  return {
-  type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-  type: 'DECREMENT'
-  }
-}
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
 let store = createStore(allReducers, 
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 //binds reducer to store
 
-store.dispatch(increment)
+
 
 ReactDOM.render(
   <Provider store = {store}>
